ef338d73b470b9a71492e6c4bc3edb839e488303
Ava <jiaranicu@outlook.com>
2021-07-23 23:15:18 +0800

    增加说明

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -1,52 +1,43 @@
 <template>
-    <div>
-        <b-navbar variant="info">
-            <b-container>
-                <b-navbar-brand to="/articles">🥰</b-navbar-brand>
-                <b-navbar-nav class="ml-auto">
-<!--                    <b-nav-item>-->
-<!--                        <b-link to="/editor">-->
-<!--                            <b-icon-pencil alt="编辑器" title="编辑器" class="nav-icon"></b-icon-pencil>-->
-<!--                        </b-link>-->
-<!--                    </b-nav-item>-->
-                    <b-nav-item v-b-modal="'modal-help'">
-                        <b-icon-question-circle title="帮助" class="nav-icon"></b-icon-question-circle>
-                    </b-nav-item>
-                </b-navbar-nav>
-            </b-container>
-        </b-navbar>
-        <b-modal id="modal-help" title="帮助" ok-only size="lg">
-            <p>做了个小网站来存放一个魂儿们的小作文和定型文，因为第一次用vue和fastapi所以非常简陋，以后会继续改进的。</p>
-            <p>欢迎大家使用jiaranicu@outlook.com投稿和联系，如果不愿意您的作品被收录也可以联系我删除。</p>
-            <hr>
-            <p>谢谢大家,谢谢大家</p>
-            <p>一个魂儿，一个魂儿在哪里？</p>
-            <p>一个魂儿！！！</p>
-            <p>这个小网站通过了嘛</p>
-            <p>我可是…vue没有准备好</p>
-            <p>我下次还会准备的</p>
-            <p>果咩拿塞</p>
-            <p>我，我写，写…我写错代码了</p>
-            <p>我，我太紧张了</p>
-            <p>果咩</p>
-            <p>对不起</p>
-            <p>我下次一定会</p>
-            <p>一定会写好的</p>
-            <p>呜呜呜…我的网站</p>
-            <p>我不耽误大家的时间了</p>
-            <p>嘤嘤</p>
-            梗来源：<a target="_blank" href="https://www.bilibili.com/video/BV1FN411o7nt">可爱小向晚</a>
-        </b-modal>
-    </div>
+  <div>
+    <b-navbar variant="info">
+      <b-container>
+        <b-navbar-brand to="/articles">🥰</b-navbar-brand>
+        <b-navbar-nav class="ml-auto">
+          <!--                    <b-nav-item>-->
+          <!--                        <b-link to="/editor">-->
+          <!--                            <b-icon-pencil alt="编辑器" title="编辑器" class="nav-icon"></b-icon-pencil>-->
+          <!--                        </b-link>-->
+          <!--                    </b-nav-item>-->
+          <b-nav-item v-b-modal="'modal-help'">
+            <b-icon-question-circle title="帮助" class="nav-icon"></b-icon-question-circle>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-container>
+    </b-navbar>
+    <b-modal id="modal-help" title="帮助" ok-only size="lg">
+      <p>做了个小网站来存放大家的小作文，主要来自B站评论区、S1、A吧，没什么标准，一般看到就会偷进来。</p>
+      <p>如果你也有想分享的作文，欢迎大家使用jiaranicu@outlook.com投稿和联系，如果不愿意您的作品被收录也可以联系我删除。</p>
+      <hr>
+      <h5>使用方法</h5>
+      <ul>
+        <li>上方搜索栏可以选择“标题”，“作者”或“标签”，按回车搜索。其中标签搜索需要输入完整的名称，
+          多个标签用空格隔开（比如“嘉然 向晚”，会显示包含这两个标签的文章）</li>
+        <li>点击小卡片上的作者或标签，也可以直接搜索</li>
+        <li>想要显示全部内容，可以 直接刷新 或者 将搜索栏清空再搜索</li>
+        <li>点击小作文标题旁的<b-icon-file-earmark-text/>可以直接复制全文</li>
+      </ul>
+    </b-modal>
+  </div>
 </template>
 
 <script>
-    import {BIconQuestionCircle} from 'bootstrap-vue'
+import {BIconQuestionCircle,BIconFileEarmarkText} from 'bootstrap-vue'
 
-    export default {
-        name: "TheNavBar",
-        components:{BIconQuestionCircle}
-    }
+export default {
+  name: "TheNavBar",
+  components: {BIconQuestionCircle,BIconFileEarmarkText}
+}
 </script>
 
 <style scoped>
